704b43c7cf555256247f0263c354dd87e5fea1cc
shongsyvilay <54946418+shongsyvilay@users.noreply.github.com>
2020-03-23 01:18:45 -0400

    Add files via upload

--- breakout.py
+++ breakout.py
@@ -5,26 +5,28 @@ import random
 import sys
 import time
 
+
 class Overlay(pygame.sprite.Sprite):
     def __init__(self):
         # Equivalent statements:
-        #pygame.sprite.Sprite.__init__(self)
+        # pygame.sprite.Sprite.__init__(self)
         super(pygame.sprite.Sprite, self).__init__()
         self.image = pygame.Surface((800, 20))
-        #self.image.fill((0, 0, 0))
+        # self.image.fill((0, 0, 0))
         self.rect = self.image.get_rect()
         self.font = pygame.font.Font('freesansbold.ttf', 18)
         self.render('Score: 0        Lives: 5')
-        
+
     def render(self, text):
         self.text = self.font.render(text, True, (255, 255, 255))
         self.image.blit(self.text, self.rect)
-    
+
     def draw(self, screen):
         screen.blit(self.text, (0, 0))
 
     def update(self, score, lives):
         self.render('Score: ' + str(score) + '        Lives: ' + str(lives))
+
 
 class Ship(pygame.sprite.Sprite):
     def __init__(self):
@@ -39,13 +41,14 @@ class Ship(pygame.sprite.Sprite):
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
+
 class Alien(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((32, 32))
         self.color = (0, 0, 0)
         self.image.fill(self.color)
-        self.vector = [ -1, 0 ]
+        self.vector = [-1, 0]
         self.start = 0
         self.rect = self.image.get_rect()
 
@@ -53,6 +56,7 @@ class Alien(pygame.sprite.Sprite):
         if self.rect.x < self.start - 100 or self.rect.x > self.start + 150:
             self.vector[0] *= -1
         self.rect.x += self.vector[0]
+
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self):
@@ -63,7 +67,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.x = 400
         self.rect.y = 485
         self.owner = 0
-        self.vector = [ 0, 0 ]
+        self.vector = [0, 0]
         self.thud_sound = pygame.mixer.Sound('assets/thud.wav')
 
     def update(self, game, aliens, ship):
@@ -78,34 +82,36 @@ class Bullet(pygame.sprite.Sprite):
             game.bullets.remove(self)
             game.score += 10
         if pygame.sprite.collide_rect(self, ship) and self.owner == 0:
-            # self.vector[1] *= -1.2
-            # self.vector[0] += random.random()
             ship.kill()
             pygame.event.post(game.new_life_event)
 
         self.rect.x += self.vector[0]
         self.rect.y += self.vector[1]
 
+class PowerUp(pygame.sprite.Sprite):
+    def __init__(self):
+        super(pygame.sprite.Sprite, self).__init__()
+        self.type = random.choice(['speed', 'explode', 'extra-life'])
+
 class Game:
     def __init__(self):
         pygame.init()
         pygame.key.set_repeat(50)
-        pygame.mixer.music.load('assets/loop.wav')
-        pygame.mixer.music.play(-1)
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((800, 600))
         self.bullets = pygame.sprite.Group()
         self.bullets.add(Bullet())
-        self.paddle = Ship()
+        self.ship = Ship()
         self.new_life_event = pygame.event.Event(pygame.USEREVENT + 1)
         self.aliens = pygame.sprite.Group()
         self.overlay = Overlay()
+        self.outro = Outro()
         self.screen.fill((0, 0, 0))
-        self.ready = True 
+        self.ready = True
         self.score = 0
-        self.lives = 4
-        for i in range(0, 4):
-            for j in range(0, 8):
+        self.lives = 1
+        for i in range(0, 1):
+            for j in range(0, 1):
                 alien = Alien()
                 alien.rect.x = j * 60 + 155
                 alien.start = alien.rect.x
@@ -124,23 +130,20 @@ class Game:
     def run(self):
         self.done = False
         last_time = time.time() - 1
-        fire_interval = random.randrange(1,4,1)
+        fire_interval = random.randrange(1, 4, 1)
         enemy_hold = time.time()
+
         while not self.done:
             self.screen.fill((0, 0, 0))
             for event in pygame.event.get():
                 if event.type == self.new_life_event.type:
                     self.lives -= 1
-                    print(self.lives)
-                    if self.lives > 0:
-                        bullet = Bullet()
-                        bullet.rect.x = self.paddle.rect.x + 23
-                        self.bullets.add(bullet)
+                    if self.lives > -1:
+                        self.ship = Ship()
                         self.ready = True
-                        print(pygame.event.get())
                     else:
-                        pygame.quit()
-                        sys.exit(0)
+                        self.outro.draw(self.screen, 'You Lose!')
+                        self.done = True
                 if event.type == pygame.QUIT:
                     self.done = True
                 if event.type == pygame.KEYDOWN:
@@ -151,53 +154,59 @@ class Game:
                             bullet.image.fill((255, 255, 0))
                             bullet.vector = [0, -1]
                             bullet.owner = 1
-                            bullet.rect.x = self.paddle.rect.x + 23
+                            bullet.rect.x = self.ship.rect.x + 23
                             self.bullets.add(bullet)
                             last_time = time.time()
                     if event.key == pygame.K_LEFT:
-                        self.paddle.rect.x -= 5
-                        if self.paddle.rect.x <= 0:
-                            self.paddle.rect.x = 0
+                        self.ship.rect.x -= 5
+                        if self.ship.rect.x <= 0:
+                            self.ship.rect.x = 0
                     if event.key == pygame.K_RIGHT:
-                        self.paddle.rect.x += 5
-                        if self.paddle.rect.x >= 750:
-                            self.paddle.rect.x = 750
-                if self.ready:
-                    self.bullets.sprites()[0].rect.x = self.paddle.rect.x + 23
+                        self.ship.rect.x += 5
+                        if self.ship.rect.x >= 750:
+                            self.ship.rect.x = 750
 
-            enemy_fire = time.time()
-            if enemy_fire - enemy_hold >= fire_interval:
-                angry_fellow = self.aliens.sprites()[random.randrange(0, len(self.aliens), 1)]
-                angry_bullet = Bullet()
-                angry_bullet.image.fill([255, 0, 0])
-                angry_bullet.vector = [0, 1]
-                angry_bullet.rect.x = angry_fellow.rect.x + 20
-                angry_bullet.rect.y = angry_fellow.rect.y + 35
-                self.bullets.add(angry_bullet)
-                enemy_hold = time.time()
-                fire_interval = random.randrange(1,4,1)
+            if len(self.aliens) == 0:
+                self.outro.draw(self.screen, 'You win!')
+                self.done = True
+            else:
+                enemy_fire = time.time()
+                if enemy_fire - enemy_hold >= fire_interval:
+                    angry_fellow = self.aliens.sprites()[random.randrange(0, len(self.aliens), 1)]
+                    angry_bullet = Bullet()
+                    angry_bullet.image.fill([255, 0, 0])
+                    angry_bullet.vector = [0, 1]
+                    angry_bullet.rect.x = angry_fellow.rect.x + 20
+                    angry_bullet.rect.y = angry_fellow.rect.y + 35
+                    self.bullets.add(angry_bullet)
+                    enemy_hold = time.time()
+                    fire_interval = random.randrange(1, 4, 1)
 
-            self.bullets.update(self, self.aliens, self.paddle)
+            self.bullets.update(self, self.aliens, self.ship)
             self.overlay.update(self.score, self.lives)
             self.aliens.update()
             self.bullets.draw(self.screen)
-            self.paddle.draw(self.screen)
+            self.ship.draw(self.screen)
             self.aliens.draw(self.screen)
             self.overlay.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(60)
 
-class Intro(pygame.sprite.Sprite):
+        time.sleep(3)
+        pygame.quit()
+        sys.exit(0)
+
+class Outro(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((800, 120))
+        self.image.fill((255, 255, 255))
         self.font = pygame.font.Font('freesansbold.ttf', 96)
-        self.text = self.font.render('Breakout!', True, (0, 0, 0))
         self.rect = self.image.get_rect()
-        self.image.blit(self.text, self.rect)
 
-    def draw(self, screen):
-        screen.blit(self.text, (0, 0))
+    def draw(self, screen, text):
+        text = self.font.render(text, True, (0, 255, 255))
+        screen.blit(text, (150, 200))
 
 if __name__ == "__main__":
     game = Game()
